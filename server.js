01323b8edfda5ff2f8a6236de916a480397faf04
Qasim Ali <qasim@Qasims-MacBook-Pro.local>
2018-09-17 11:01:27 +0500

    draft backup with willingness page not working

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const hbs = require('hbs');
 
 const {mongoose} = require('./db/mongoose');
 const {Friends} = require('./models/friends');
 const {Abilities} = require('./models/abilities');
 
 var app = express();
+app.use(express.static(__dirname+'/static'));
 app.use(bodyParser.json());
+app.set('view engine','hbs');
+
+app.get('/',(req,res) => {
+  res.render('index.hbs');
+})
+
+app.get('/login',(req,res) => {
+  res.render('login.hbs');
+})
+
+app.get('/signup',(req,res) => {
+  res.render('signup.hbs',{
+    name: 'Qasim Ali',
+    phone: +923235168638
+  });
+})
+
+app.get('/willingess',(req,res) => {
+  res.status(200).render('willingess.hbs');
+})
 
 app.post('/locals',(req,res) => {
   var afriend = new Friends({
