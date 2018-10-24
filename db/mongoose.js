69985a9c890b8b45b735133a283412838633813f
Qasim Ali <qasimali24@gmail.com>
2018-10-24 22:43:33 +0500

    Final hopefully ready for heroku.

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Downloads',{
+mongoose.connect(process.env.MONGODB_URI,{
   useNewUrlParser: true,
 });
 mongoose.set('useCreateIndex', true);
